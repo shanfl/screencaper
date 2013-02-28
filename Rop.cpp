906156c6263f4148596a640f9eff9bfcc3320f3f
lyricc <lyricc@sina.cn>
2013-02-28 19:03:08 +0800

    add srcs

---
+++ Rop.cpp
@@ -0,0 +1,33 @@
+#include "RopWindow.h"
+CAppModule _Module;
+#pragma comment( lib, "gdiplus.lib" )
+int WINAPI WinMain( IN HINSTANCE hInstance, IN HINSTANCE hPrevInstance, IN LPSTR lpCmdLine, IN int nShowCmd )
+{
+
+	_Module.Init(0,hInstance);
+	HRESULT hRes = ::CoInitialize(NULL);
+	AtlInitCommonControls(ICC_COOL_CLASSES | ICC_BAR_CLASSES); 
+	ULONG_PTR m_gdiplusToken;
+	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
+	Gdiplus::GdiplusStartup(&m_gdiplusToken, &gdiplusStartupInput, NULL);
+
+	MSG uMsg;
+
+	RopWindow wnd;
+	wnd.Create(NULL,CWindow::rcDefault,"Rop",WS_VISIBLE | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_OVERLAPPEDWINDOW);
+
+	wnd.ShowWindow(TRUE);
+	wnd.UpdateWindow();
+
+
+	while(GetMessage(&uMsg,0,0,0))
+	{
+		TranslateMessage(&uMsg);
+		DispatchMessage(&uMsg);
+	}
+
+
+	_Module.Term();
+	Gdiplus::GdiplusShutdown(m_gdiplusToken);
+	return 0;
+}
---
+++ stdafx.h
@@ -0,0 +1,22 @@
+#ifndef _STDAFX_H_
+#define _STDAFX_H_
+
+
+// Change these values to use different versions
+#define WINVER            0x0501
+#define _WIN32_WINNT      0x0501
+#define _WIN32_IE         0x0600
+#define _RICHEDIT_VER     0x0100
+
+#include <atlbase.h>
+#include <atlapp.h>
+
+extern CAppModule _Module;
+
+
+#include <atlwin.h>
+#include <atlmisc.h>
+
+#include <GdiPlus.h>
+
+#endif
